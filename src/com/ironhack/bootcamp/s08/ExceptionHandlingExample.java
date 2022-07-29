eb35e5fb07da97e89f5c16e61731549e93952de1
amr <anma@manning.com>
2022-07-29 18:12:12 +0200

    Example of how Java handles exceptions

---
+++ src/com/ironhack/bootcamp/s08/ExceptionHandlingExample.java
@@ -0,0 +1,9 @@
+package com.ironhack.bootcamp.s08;
+
+public class ExceptionHandlingExample {
+
+    public static void main(String[] args) {
+        Level1 level1 = new Level1();
+        level1.treeCall1();
+    }
+}
---
+++ src/com/ironhack/bootcamp/s08/Level3.java
@@ -0,0 +1,7 @@
+package com.ironhack.bootcamp.s08;
+
+public class Level3 {
+    public void treeCall3() {
+        Float floatThrowingNumberFormatException = Float.parseFloat("a");
+    }
+}
