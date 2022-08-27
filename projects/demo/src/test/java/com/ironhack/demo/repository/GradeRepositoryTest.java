627216c371e73aa6cd8370d1ce5aa3f31ff2fd2c
amr <dquijote22@gmail.com>
2022-08-27 09:54:38 +0200

    possible solution for tests

--- projects/demo/src/test/java/com/ironhack/demo/repository/GradeRepositoryTest.java
+++ projects/demo/src/test/java/com/ironhack/demo/repository/GradeRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.ironhack.demo.repository;
 
 import com.ironhack.demo.model.Grade;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,40 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class GradeRepositoryTest {
 
+    public static final String SECTION_ID = "CS901-A";
     @Autowired
     private GradeRepository gradeRepository;
 
+    private Grade grade1;
+    private Grade grade2;
+    private Grade grade3;
+
     @BeforeEach
     void setUp() {
+        gradeRepository.deleteAll();
+        grade1 = new Grade();
+        grade1.setStudentName("Juan Elías");
+        grade1.setSectionId(SECTION_ID);
+        grade1.setScore(102);
+        gradeRepository.save(grade1);
+
+        grade2 = new Grade();
+        grade2.setStudentName("Juan Elías");
+        grade2.setSectionId(SECTION_ID);
+        grade2.setScore(102);
+        gradeRepository.save(grade2);
+
+        grade3 = new Grade();
+        grade3.setStudentName("Juan Elías");
+        grade3.setSectionId(SECTION_ID);
+        grade3.setScore(102);
+        gradeRepository.save(grade3);
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        gradeRepository.deleteAll();
     }
 
     @Test
@@ -37,32 +67,26 @@ class GradeRepositoryTest {
 
     @Test
     void findAll() {
-        Iterable<Grade> response = gradeRepository.findAll();
-        int i = 0;
-        for (Grade item: response) {
-           i++;
-        }
-        assertEquals(8, i);
+        Long count = gradeRepository.count();
+        assertEquals(3, count);
     }
 
     @Test
     void update() {
-        Grade grade = gradeRepository.findById(1).get();
-        grade.setScore(95);
-        gradeRepository.save(grade);
+        grade1.setScore(95);
+        gradeRepository.save(grade1);
 
 
-        Grade savedGrade = gradeRepository.findById(1).get();
+        Grade savedGrade = gradeRepository.findById(grade1.getId()).get();
         assertEquals(95, savedGrade.getScore());
 
     }
 
     @Test
     void delete() {
-        Grade grade = gradeRepository.findById(1).get();
-        gradeRepository.delete(grade);
+        gradeRepository.delete(grade1);
 
-        assertFalse(gradeRepository.findById(1).isPresent());
+        assertFalse(gradeRepository.findById(grade1.getId()).isPresent());
 
     }
 }
